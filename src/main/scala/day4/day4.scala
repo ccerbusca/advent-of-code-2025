a6eee72948592fe0252e1ed65520507c29f0b42e
Cristian Cerbusca <cristian.cerbusca@gmail.com>
2025-12-04 18:18:04 +0200

    Day 4

---
+++ src/main/scala/day4/day4.scala
@@ -0,0 +1,97 @@
+package day4
+
+import scala.annotation.tailrec
+import scala.util.chaining.*
+
+val coordinates = {
+  val set = for {
+    a <- Set(0, -1, 1)
+    b <- Set(0, -1, 1)
+  } yield (a, b)
+  set.excl((0, 0))
+}
+
+def checkSurroundings(
+  occupiedCells: Set[(Int, Int)],
+  rowCount: Int,
+  columnCount: Int,
+) =
+  occupiedCells.filter { case (i, j) =>
+    coordinates
+      .map { case (x, y) => (i + x) -> (j + y) }
+      .filterNot { case (i, j) => i < 0 || i >= rowCount || j < 0 || j >= columnCount }
+      .intersect(occupiedCells)
+      .size < 4
+  }
+
+def part1(): Unit =
+  val source = io.Source.fromResource("day4.txt")
+  val mat = source
+    .getLines()
+    .to(LazyList)
+    .map { line =>
+      line.toVector
+    }
+    .toVector
+
+  val occupiedCells = mat.zipWithIndex.foldLeft(Set.empty[(Int, Int)]) { case (set, (row, i)) =>
+    val rowSet = row.zipWithIndex.foldLeft(Set.empty[(Int, Int)]) { case (rowSet, (cell, j)) =>
+      if cell == '@' then rowSet.incl((i, j)) else rowSet
+    }
+    set ++ rowSet
+  }
+
+  source.close()
+  
+  checkSurroundings(
+    occupiedCells = occupiedCells,
+    rowCount = mat.size,
+    columnCount = mat.head.size
+  ).size.tap(println)
+
+@tailrec
+def checkRemovable(
+  occupiedCells: Set[(Int, Int)],
+  rowCount: Int,
+  columnCount: Int,
+  totalCount: Int = 0
+): Int =
+  val removable = checkSurroundings(occupiedCells, rowCount, columnCount)
+  if removable.nonEmpty then
+    checkRemovable(
+      occupiedCells.diff(removable),
+      rowCount,
+      columnCount,
+      totalCount + removable.size
+    )
+  else
+    totalCount
+
+def part2(): Unit =
+  val source = io.Source.fromResource("day4.txt")
+  val mat = source
+    .getLines()
+    .to(LazyList)
+    .map { line =>
+      line.toVector
+    }
+    .toVector
+
+  val occupiedCells = mat.zipWithIndex.foldLeft(Set.empty[(Int, Int)]) { case (set, (row, i)) =>
+    val rowSet = row.zipWithIndex.foldLeft(Set.empty[(Int, Int)]) { case (rowSet, (cell, j)) =>
+      if cell == '@' then rowSet.incl((i, j)) else rowSet
+    }
+    set ++ rowSet
+  }
+
+  source.close()
+
+  checkRemovable(
+    occupiedCells = occupiedCells,
+    rowCount = mat.size,
+    columnCount = mat.head.size
+  ).tap(println)
+
+@main
+def main(): Unit =
+  part2()
